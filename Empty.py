d18db41c47ebb3c34631051cada7f0c5bd156990
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2023-12-05 14:56:45 -0500

    Update Empty.py

--- Empty.py
+++ Empty.py
@@ -7,5 +7,9 @@ elif x=="t":
     print("I'm tired")
 elif x=="s":
     print("I'm sad")
-elif x=="a":
+elif x=="an":
     print("I'm annoyed")
+elif x=="a":
+    print("I'm angry")
+elif x=="":
+    print("")
