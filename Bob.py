69b765b1708f10eba98467ba3a500383e134268d
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2024-08-29 15:07:19 -0400

    2 Number Calculator

--- Bob.py
+++ Bob.py
@@ -5,23 +5,23 @@ print("3. Multiplication")
 print("4. Division")
 print("5. Exit")
 x=input("")
-if x==1:
+if x=="1":
     y=input("Number 1")
     z=input("Number 2")
     print(y+z)
-elif x==2:
+elif x=="2":
     y=input("Number 1")
     z=input("Number 2")
     print(y-z)
-elif x==3:
+elif x=="3":
     y=input("Number 1")
     z=input("Number 2")
     print(y*z)
-elif x==4:
+elif x=="4":
     y=input("Number 1")
     z=input("Number 2")
     print(y/z)
-elif x==5:
+elif x=="5":
     print("Bye!")
 else:
     print("Error")
