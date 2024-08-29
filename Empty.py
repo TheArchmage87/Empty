a57ff2a90eb12b418cf9681417e005067aab38b2
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2024-08-29 18:22:42 +0000

    8ball

--- Empty.py
+++ Empty.py
@@ -1,4 +1,5 @@
 import random
 Responses=["It is certain", "Reply hazy, try again", "Don’t count on it", "It is decidedly so", "Ask again later", "My reply is no", "Without a doubt", "Better not tell you now", "My sources say no", "Yes definitely", "Cannot predict now", "Outlook not so good", "You may rely on it", "Concentrate and ask again", "Very doubtful", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes"]
-Random_Value=random(1, 21)
+Random_Value=random.randint(1, 21)
+input("Question?")
 print(Responses[Random_Value])
