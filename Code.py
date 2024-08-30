b8a99bc6fa8568e942738b6045a2cbc248e13133
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2024-08-30 10:30:19 -0400

    bob

---
+++ Code.py
@@ -0,0 +1,10 @@
+tasks = []
+def display_menu():
+    print("To-Do List Menu:")
+    print("1. Add a task")
+    print("2. Remove a task")
+    print("3. View all tasks")
+    print("4. Exit")
+def add_task():
+    
+display_menu()
