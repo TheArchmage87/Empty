1a6670d1b9eef7072748ae05bd077c2a126c385a
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2024-08-30 13:24:21 -0400

    bob

--- Code.py
+++ Code.py
@@ -1,10 +1,17 @@
-tasks = []
+tasks=[]
+list_length=0
+def add_task():
+    print("Type task or type exit to return to menu.")
+    task_add=input("")
+    if task_add=="exit" or task_add=="Exit":
+        display_menu()
 def display_menu():
     print("To-Do List Menu:")
     print("1. Add a task")
     print("2. Remove a task")
     print("3. View all tasks")
     print("4. Exit")
-def add_task():
-    
+    x=input("")
+    if x==1:
+        add_task()
 display_menu()
